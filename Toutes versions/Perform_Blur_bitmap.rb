7eb42a2201c3b221428d55140e421e36d3971906
Funkywork <funkywork.courriel@gmail.com>
2012-09-07 02:56:20 +0200

    Update Perform Blur

---
+++ Toutes versions/Perform_Blur_bitmap.rb
@@ -0,0 +1,31 @@
+# Perform Blur by Hiino and Grim and Zeus81
+#==============================================================================
+# ** Bitmap
+#------------------------------------------------------------------------------
+# Add the perform_blur function
+#==============================================================================
+ 
+class Bitmap
+  #--------------------------------------------------------------------------
+  # * Blur a bitmap
+  #--------------------------------------------------------------------------
+  def perform_blur(decal)
+    clone_bmp = self.clone
+    8.times do |index|
+      case index
+      when 0; x, y = -decal, decal
+      when 1; x, y = 0, decal
+      when 2; x, y = 0, -decal
+      when 3; x, y = -decal, -decal
+      when 4; x, y = decal, decal
+      when 5; x, y = -decal, 0
+      when 6; x, y = decal, 0
+      when 7; x, y = decal, -decal
+      end
+      fact_opacity = (index == 8) ? 255/2 : 255/(index+1)
+      self.blt(x, y, clone_bmp, rect, fact_opacity)
+    end
+    clone_bmp.dispose
+  end
+ 
+end
